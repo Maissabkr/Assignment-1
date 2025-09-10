70ea85ab7b283512a38768439c85229cd8664ec6
Maissabkr <mb10038@nyu.edu>
2025-09-10 23:23:13 +0800

    max_profit.py

--- max_profit.py
+++ max_profit.py
@@ -1,6 +1,6 @@
 def max_profit(prices):
     if not prices or len(prices)<2:
-        return 0
+        return (False,0)
     min_prices=prices[0]
     max_profit=0
     for price in prices[1:]:
@@ -9,7 +9,7 @@ def max_profit(prices):
             max_profit=profit
         if price<min_price:
             min_price=price
-    return max_profit
+    return (max_profit>0,max_profit)
 
 if __name__=="__main__":
     prices=[800,300,600,200,100]
