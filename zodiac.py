ce102ecb008c2c6fbe53b0afed4061369c7bafdb
Maissabkr <mb10038@nyu.edu>
2025-09-10 23:28:21 +0800

    zodiac.py

--- zodiac.py
+++ zodiac.py
@@ -1,14 +1,14 @@
-zodiac_animals=['Monkey','Rooster','Dog','Pig','Rat','Ox','Tiger','Hare','Dragon','Snake','Horse','Goat']
+zodiac_animals=["Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Hare","Dragon","Snake","Horse","Goat"]
 while True:
     your_year=input('Enter a year:')
     if not your_year.isdigit():
         print('invalid, please enter a non negative integer')
         continue
     year=int(your_year)
-    if year<=0:
+    if year<0:
         print('Input positive year')
         continue
     animal_index=year%12
-    animals=zodiac_animals[animal_index]
-    print(f'The year of {year} is the animal of {animals}' )
+    animal=zodiac_animals[animal_index]
+    print(f"The year of {year} is the animal of {animal}")
     break
